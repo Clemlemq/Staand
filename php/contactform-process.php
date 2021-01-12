d767a31a0dd3bab060d78428c9f7274918a44982
Clément Lemaire <clement@charrette.club>
2021-01-12 15:08:24 +0100

    first commit

---
+++ php/contactform-process.php
@@ -0,0 +1,93 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+$EmailTo = "clement@charrette.club";
+$Subject = "New Form APV";
+
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+
+?>
+
+
+<!-- <?php
+// $errorMSG = "";
+
+// if (empty($_POST["name"])) {
+//     $errorMSG = "Name is required ";
+// } else {
+//     $name = $_POST["name"];
+}
+
+// if (empty($_POST["email"])) {
+//     $errorMSG = "Email is required ";
+// } else {
+//     $email = $_POST["email"];
+// }
+
+// if (empty($_POST["message"])) {
+//     $errorMSG = "Message is required ";
+// } else {
+//     $message = $_POST["message"];
+// }
+
+// if (empty($_POST["terms"])) {
+//     $errorMSG = "Terms is required ";
+// } else {
+//     $terms = $_POST["terms"];
+}
+
+// $EmailTo = "yourname@domain.com";
+// $Subject = "New message from Leno landing page";
+
+// // prepare email body text
+// $Body = "";
+// $Body .= "Name: ";
+// $Body .= $name;
+// $Body .= "\n";
+// $Body .= "Email: ";
+// $Body .= $email;
+// $Body .= "\n";
+// $Body .= "Message: ";
+// $Body .= $message;
+// $Body .= "\n";
+// $Body .= "Terms: ";
+// $Body .= $terms;
+// $Body .= "\n";
+
+// send email
+// $success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// // redirect to success page
+// if ($success && $errorMSG == ""){
+//    echo "success";
+// }else{
+//     if($errorMSG == ""){
+//         echo "Something went wrong :(";
+//     } else {
+//         echo $errorMSG;
+    }
+}
+?> -->
